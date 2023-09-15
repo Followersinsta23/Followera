bd3018620dd90754f0b35686b4890b517c81427f
Followersinsta23 <145121527+Followersinsta23@users.noreply.github.com>
2023-09-15 17:33:24 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,5 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  //   copy your firebase config informations
+  apiKey: "AIzaSyDuO98E5pRr2gXCVITEbnkankxXGO3_YZI",
+  authDomain: "jets-eeafd.firebaseapp.com",
+  projectId: "jets-eeafd",
+  storageBucket: "jets-eeafd.appspot.com",
+  messagingSenderId: "619913777499",
+  appId: "1:619913777499:web:6a9f147a254cdad9a80a7e",
+  measurementId: "G-87ZHPHVTMZ"
 };
 
 // initialize firebase
