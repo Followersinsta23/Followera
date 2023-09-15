961264b3dee3d763bdc4b2a3e9ff681aeae204f7
Followersinsta23 <145121527+Followersinsta23@users.noreply.github.com>
2023-09-15 18:09:29 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,6 +1,13 @@
-
-https://followersinsta23.github.io/Followera/?
-// initialize firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBDRHfwn2yE8VL_WuXqkrXozMGv8uCfSUQ",
+  authDomain: "followers-c2e76.firebaseapp.com",
+  projectId: "followers-c2e76",
+  storageBucket: "followers-c2e76.appspot.com",
+  messagingSenderId: "1050483012186",
+  appId: "1:1050483012186:web:f5c52621e59f66ded4de63",
+  measurementId: "G-CEC951PXCR"
+};
 firebase.initializeApp(firebaseConfig);
 
 // reference your database
